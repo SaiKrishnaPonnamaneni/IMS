d3b169eb515c9415eb916b8b0bc126df7be63782
newschoolapp_2219-admin <saikrishnab2219@gmail.com>
2024-12-18 19:50:22 +0530

    Token Storage Implemeted

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,15 +1,18 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Token } from '@angular/compiler';
+import { Token } from '../models/token'; // Adjust the import path as necessary
+import { Login } from '../models/login';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private apiUrl = 'https://reqres.in/api/login'; // Your API URL
 
-  constructor(private httpClient : HttpClient) {}
-    doLogin(data:Token):Observable<any>{
-    return this.httpClient.post(" https://62b9299dff109cd1dc8ca34f.mockapi.io/students",data);
-     }
-   }
+  constructor(private httpClient: HttpClient) {}
+
+  doLogin(payLoad: Login): Observable<Token> {
+    return this.httpClient.post<Token>(this.apiUrl, payLoad);
+  }
+}
