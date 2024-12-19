40795f83d215baf99b10d7088c766c78867b4d1c
newschoolapp_2219-admin <saikrishnab2219@gmail.com>
2024-12-19 11:29:32 +0530

    Form implemented with personal details

--- src/app/components/create-student/create-student.component.ts
+++ src/app/components/create-student/create-student.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-student',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-student.component.scss']
 })
 export class CreateStudentComponent {
-
+public studentForm:FormGroup = new FormGroup(
+  {
+    name: new FormControl(),
+    gender: new FormControl(),
+    mobile: new FormControl(),
+    email: new FormControl(),
+    batch: new FormControl(),
+  }
+)
 }
